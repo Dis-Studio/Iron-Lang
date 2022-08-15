686139e7789b8e7befede23babe467dc0180bd4b
HONAK <88250329+HONAK0@users.noreply.github.com>
2022-08-15 18:04:58 +0500

    fix color text

--- src/lang/io/err.js
+++ src/lang/io/err.js
@@ -1,5 +1,5 @@
 function showerr(msg, type){
-    console.log(`\x1b[31mERROR\x1b[0m:\x1b[36m:${type}\x1b[0m::\x1b[32m${msg}\x1b[0m`);
+    console.log(`\x1b[31mERROR\x1b[0m::\x1b[36m${type}\x1b[0m::\x1b[32m${msg}\x1b[0m`);
     process.exit(msg.split("").length);
 }
 module.exports = {
