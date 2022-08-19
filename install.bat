87c38bb52732724b47f227113428e850bdc68469
HONAK <88250329+HONAK0@users.noreply.github.com>
2022-08-19 20:44:35 +0500

    fix

--- install.bat
+++ install.bat
@@ -2,4 +2,4 @@ echo "Transferring files to the build directory..."
 move index-linux build/Iron-Lang-linux
 move index-macos build/Iron-Lang-macos
 move index-win.exe build/Iron-Lang-win.exe
-copy build/Iron-Lang-win.exe C:/Windows/system32/ironlang.exe
+copy build\Iron-Lang-win.exe C:\Windows\system32\ironlang.exe
